6b067f535804082a5cbb5be7b5ec97080382b201
gasher <69788442+RmnSoni@users.noreply.github.com>
2023-09-21 02:17:31 +0530

    added public api paths, uploading to vercel

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -23,7 +23,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
   const [showEdit, setShowEdit] = useState(false)
   const getUser = async () => {
-    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+    const response = await fetch(`https://sharespeare-backend.adaptable.app/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
